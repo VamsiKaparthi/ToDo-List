13d9ea5e2626b95cb746503a59134ebc1a8effa6
Vamsi <vk784@snu.edu.in>
2023-06-01 19:25:03 +0530

    Add dark-light mode toggle in header

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import './style.css';
-import 'boxicons'
+import 'boxicons';
+
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
+const icon = document.querySelector('i')
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-brightness-high-fill');
+    if(this.classList.toggle('bi-moon-fill')){
+        body.style.background = 'antiquewhite';
+        body.style.color = 'black';
+        body.style.transition = '0.5s';
+        icon.style.color = 'black'
+        
+    }else{
+        body.style.background = 'black';
+        body.style.color = 'white';
+        body.style.transition = '0.5s';
+        icon.style.color = '#ca4726';
+    }
+});
