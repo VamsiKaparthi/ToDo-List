48c3538ccb3f1f23f9025ea58fc23cb5e1ac7661
Vamsi <vk784@snu.edu.in>
2023-06-01 20:12:22 +0530

    Add dark light mode toggle module

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
 import './style.css';
 import 'boxicons';
-
+import f from './functions/toggleDark';
+//dark light mode toggle
 const toggle = document.getElementById('toggleDark');
-const body = document.querySelector('body');
-const icon = document.querySelector('i')
-toggle.addEventListener('click', function(){
-    this.classList.toggle('bi-brightness-high-fill');
-    if(this.classList.toggle('bi-moon-fill')){
-        body.style.background = 'antiquewhite';
-        body.style.color = 'black';
-        body.style.transition = '0.5s';
-        icon.style.color = 'black'
-        
-    }else{
-        body.style.background = 'black';
-        body.style.color = 'white';
-        body.style.transition = '0.5s';
-        icon.style.color = '#ca4726';
-    }
-});
+toggle.addEventListener('click',f);
