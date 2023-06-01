96e1b4ce36ae98a43c5818e1fb87e4d7c63979fc
Vamsi <vk784@snu.edu.in>
2023-06-01 20:33:58 +0530

    Add quoteGen function in headerFunctions.js module

--- src/functions/headerFunctions.js
+++ src/functions/headerFunctions.js
@@ -1,5 +1,5 @@
 
-////dark light mode toggle
+//dark light mode toggle
 const body = document.querySelector('body');
 const icon = document.querySelector('i');
 const f = function(){
@@ -16,4 +16,18 @@ const f = function(){
         icon.style.color = '#ca4726';
     }
 }
-export default f;
+
+//Generate a random quote
+const quoteGen = function(){
+    const quote = document.querySelector('.quote');
+    const inspiringQuotes = [
+        "Believe you can achieve greatness.",
+        "Dream big, work hard.",
+        "Embrace the journey, find joy.",
+        "Chase your passions fearlessly.",
+        "Never stop learning, growing."
+      ];
+    let x = Math.floor((Math.random()*5));
+    quote.innerText = inspiringQuotes[x];
+}
+export {f,quoteGen};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import './style.css';
 import 'boxicons';
-import f from './functions/toggleDark';
+import {f,quoteGen} from './functions/headerFunctions.js';
+
+//Header
+
 //dark light mode toggle
 const toggle = document.getElementById('toggleDark');
 toggle.addEventListener('click',f);
+
+//Quote Generator
+quoteGen()
+
+//Control
