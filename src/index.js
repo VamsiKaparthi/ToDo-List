8728fdbde022b88b42edac0afeeb74fcc26938b8
Vamsi <vk784@snu.edu.in>
2023-06-02 20:52:17 +0530

    Add functionality to add taskObjects to inbox array

--- src/index.js
+++ src/index.js
@@ -12,14 +12,42 @@ toggle.addEventListener('click',f);
 quoteGen()
 
 //Control
+let inbox = [];
+let taskObj = (TaskName,Description,DueDate,Priority)=>{
+    return{TaskName,Description,DueDate,Priority};
+};
 let add = document.querySelector('.add')
 let form = document.getElementById('popup-form')
 add.addEventListener('click',()=>{
     add.style.display='none';
     form.style.display='flex';
 })
-
+let cancel = document.getElementById('cancel')
+cancel.addEventListener('click',()=>{
+    form.style.display='none';
+    add.style.display='flex';
+})
 let submit = document.getElementById('submit')
 submit.addEventListener('click',(e)=>{
     e.preventDefault()
+    
+    let taskName = document.getElementById('Task-Name');
+    let desc = document.getElementById('desc');
+    let due = document.getElementById('due');
+    let priority = document.getElementById('priorities');
+
+    if(!taskName||!due||!priority){
+        alert("Ensure you fill in all fields");
+    }
+    else{
+        let task = taskObj(taskName.value,desc.value,due.value,priority.value);
+        inbox.push(task);
+        console.log(inbox);
+        taskName.value = "";
+        desc.value = "";
+        due.value="";
+        priority.value="";
+        form.style.display='none';
+        add.style.display='flex';
+    }
 })
